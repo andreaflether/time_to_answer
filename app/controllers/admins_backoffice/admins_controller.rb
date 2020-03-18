a80445b4917592cb3401f43bf00e5f2502d18dc9
Andréa Alencar <andreaflether@gmail.com>
2020-03-18 17:34:35 -0300

    refatoração no controller e criação da página edit (admin)

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,5 +1,36 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :set_admin, only: [:edit, :update]
+  before_action :verify_password, only: [:update]
+
   def index
     @admins = Admin.all
   end
+
+  def edit
+    
+  end
+  
+  def update 
+    if @admin.update(admin_params)
+      redirect_to admins_backoffice_admins_path, notice: "Informações de administrador atualizadas com sucesso."
+    else  
+      render :edit
+    end 
+  end 
+
+  def verify_password 
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank? 
+      params[:admin].extract!(:password, :password_confirmation)
+    end 
+  end 
+
+  private 
+
+  def set_admin 
+    @admin = Admin.find(params[:id])
+  end 
+
+  def admin_params 
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end 
 end
