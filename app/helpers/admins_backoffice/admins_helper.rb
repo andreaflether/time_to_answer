c78e19da648165e5879f3279eca030fd1220c9ae
Andréa Alencar <andreaflether@gmail.com>
2020-03-20 09:17:23 -0300

    generalizando i18n nas views

--- app/helpers/admins_backoffice/admins_helper.rb
+++ app/helpers/admins_backoffice/admins_helper.rb
@@ -1,2 +1,7 @@
 module AdminsBackoffice::AdminsHelper
+
+  def translate(object = nil, method = nil)
+    (object && method) ? object.model.human_attribute_name(method) : "Tradução não localizada"
+  end 
+
 end
