5e977f805e1a3c333dc18ebcd492ea8d2e9ee3b2
Andréa Alencar <andreaflether@gmail.com>
2020-03-22 12:58:47 -0300

    add estilos diferentes às págs principais do site, add kaminari e pesquisa funcional

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,2 +1,11 @@
-class Site::SearchController < ApplicationController
+class Site::SearchController < SiteController
+  def questions 
+    if params[:term]
+      @questions = Question.includes(:answers, :subject)
+                            .where("description LIKE ?", "%#{params[:term].downcase}%")
+                            .page(params[:page]).per(5)
+    else 
+      @questions = Question.includes(:answers, :subject).last(5)
+    end 
+  end 
 end
