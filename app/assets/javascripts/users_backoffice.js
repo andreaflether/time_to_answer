73efc45d04a066a70ba029b33ce2cde09effcabb
Andréa Alencar <andreaflether@gmail.com>
2020-03-24 20:10:52 -0300

    add datetimepicker na data de nascimento e radio button no gênero

--- app/assets/javascripts/users_backoffice.js
+++ app/assets/javascripts/users_backoffice.js
@@ -3,3 +3,12 @@
 //= require bootstrap/dist/js/bootstrap.bundle
 //= require sb-admin-2
 //= require lmat.bs.growl/jquery.bootstrap-growl
+//= require jquery-datetimepicker/build/jquery.datetimepicker.full
+
+$(document).ready(function() {
+  $('#datetimepicker').datetimepicker({
+    timepicker: false,
+    format: 'd/m/Y'
+  });
+  $.datetimepicker.setLocale('pt-BR');
+})
