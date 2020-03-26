13513beab9c0d969a5d7010a12aeebe3893ecabc
Andréa Alencar <andreaflether@gmail.com>
2020-03-25 22:39:49 -0300

    add estatísticas de admin

--- app/controllers/admins_backoffice/welcome_controller.rb
+++ app/controllers/admins_backoffice/welcome_controller.rb
@@ -1,4 +1,7 @@
 class AdminsBackoffice::WelcomeController < AdminsBackofficeController
   def index
+    @total_users = AdminStatistic.total_users.value
+    @total_subjects = AdminStatistic.total_subjects.value
+    @total_questions = AdminStatistic.total_questions.value
   end
 end
