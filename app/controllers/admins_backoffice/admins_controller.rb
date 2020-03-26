6f641bad19f0abaf540bf6ba120516a3f9bbceb9
Andréa Alencar <andreaflether@gmail.com>
2020-03-26 12:53:53 -0300

    criando um mailer ao att admin

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -12,6 +12,7 @@ class AdminsBackoffice::AdminsController < AdminsBackofficeController
   
   def update 
     if @admin.update(admin_params)
+      AdminMailer.update_email(current_admin, @admin).deliver_now
       redirect_to admins_backoffice_admins_path, notice: "Informações atualizadas com sucesso."
     else  
       render 'edit'
